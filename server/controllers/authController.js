422e199d5e5edb61088e499b24f6c3d5be3a0398
Shivasankarisharmi <shivasankarisharmi@gmail.com>
2026-03-18 17:30:45 +0530

    switch to Brevo SMTP

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -53,8 +53,8 @@ const forgotPassword = async (req, res) => {
       port: process.env.EMAIL_PORT,
       secure: false,
       auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
       }
     })
 
